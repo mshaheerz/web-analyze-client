2c1f4ceb86e5727b663664adfc774701c094ea91
shaheerez <72137242+mshaheerz@users.noreply.github.com>
2023-07-04 16:55:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 
-axios.defaults.baseURL='http://127.0.0.1:3000'
+axios.defaults.baseURL='https://api-anlyz.onrender.com/'
 axios.defaults.withCredentials =true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
