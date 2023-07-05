0d474b8a452dee944716f31cc60af5ae492721b9
shaheerez <72137242+mshaheerz@users.noreply.github.com>
2023-07-05 10:08:01 +0530

    some fix

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -38,7 +38,7 @@ function RegisterPage() {
         <section className="bg-gray-50  h-screen">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-pink-600">
-                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    <img className="w-8 h-8 mr-2" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Google_Chrome_icon_%282011%29.png" alt="logo" />
                     Web Analyzer
                 </a>
                 <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
